c95e24ac68d9ac5506a745ea4495a42f12a66c84
abdulwahid31 <wahid_@student.ub.ac.id>
2022-04-13 10:41:27 +0700

    PHP lanjutan

---
+++ Lanjutan PHP 13 April/php-lanjutan.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nama = "Browny";
+    $umur = 1000;
+    $tinggi = 3;
+    $kalimat = "Halo, nama saya $nama umur saya adalah $umur tahun dan tinggi saya adalah $tinggi";
+
+    echo $kalimat;
+?>
